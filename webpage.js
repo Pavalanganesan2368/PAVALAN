f5bdb7b4134e924e7e9ab0328b48a409baa8d37c
Pavalanganesan2368 <136297958+Pavalanganesan2368@users.noreply.github.com>
2023-06-13 16:15:19 +0530

    ADD HTML

---
+++ webpage.js
@@ -0,0 +1,7 @@
+var cursor1=document.querySelector(".cursor1");
+var cursor2=document.querySelector(".cursor2");
+document.addEventListener("mouse move".function(e))
+{
+    cursor1.style.csstext=cursor2.style.csstext="left:"+e.clientX+"px;top:"+e.clientY+"px;";3.;
+}
+
